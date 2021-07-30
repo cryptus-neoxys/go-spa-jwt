bc6b2755a24913c0c7fd470c9eb3e2146d44499a
cryptus-neoxys <sharma.dev4242@gmail.com>
2021-07-30 20:44:14 +0530

    bigFIX ✨: add CORS config

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"github.com/cryptus-neoxys/go-spa-jwt/database"
 	"github.com/cryptus-neoxys/go-spa-jwt/routes"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 func main() {
@@ -11,6 +12,10 @@ func main() {
 	database.Connect()
 
 	app := fiber.New()
+
+	app.Use(cors.New(cors.Config{
+		AllowCredentials: true,
+	}))
 
 	routes.Setup(app)
 
